efa5bd876bdd6f2abed85f2463c4eae3f9714cb6
Mark Barcinski <barcinski@gmail.com>
2026-04-14 13:47:44 +0200

    radios

---
+++ works/files/filters.js
@@ -0,0 +1,35 @@
+document.write('<form>\
+    <input name="s" type="radio" id="forsale" onchange="showForsale()"><label for="forsale">For Sale</label>\
+    <input name="s" type="radio" id="archive" onchange="showSold()"><label for="archive">Archive</label>\
+    <input name="s" type="radio" id="all" onchange="showAll()" checked><label for="all">All</label>\
+</form>');
+
+function showForsale(){
+    const o = document.getElementsByClassName("item")
+
+    for ( const  foo of o){
+        //console.log( getComputedStyle(foo).display)
+
+        if(foo.hasAttribute("sold"))foo.style.display = 'none';
+        else foo.style.display = 'inline-table';
+    }
+}
+
+function showSold(){
+    const o = document.getElementsByClassName("item")
+
+    for ( const  foo of o){
+        if(foo.hasAttribute("sold"))foo.style.display = 'inline-table';
+        else foo.style.display = 'none';
+    }   
+}
+
+function showAll(){
+    const o = document.getElementsByClassName("item")
+
+    for ( const  foo of o){
+        foo.style.display = 'inline-table';
+        
+    }
+
+}
