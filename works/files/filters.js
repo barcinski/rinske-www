8cf52c4ca296c48b976338f09ac1e0e5ebd87a2b
Mark Barcinski <barcinski@gmail.com>
2026-04-16 23:39:06 +0200

    descriptions

--- works/files/filters.js
+++ works/files/filters.js
@@ -1,8 +1,10 @@
+return
 document.write('<form>\
     <input name="s" type="radio" id="forsale" onchange="showForsale()"><label for="forsale">For Sale</label>\
     <input name="s" type="radio" id="archive" onchange="showSold()"><label for="archive">Archive</label>\
     <input name="s" type="radio" id="all" onchange="showAll()" checked><label for="all">All</label>\
-</form>');
+    ');
+//</form>');
 
 function showForsale(){
     const o = document.getElementsByClassName("item")
@@ -32,4 +34,28 @@ function showAll(){
         
     }
 
+}
+
+document.write('\
+    <input name="s" type="radio" id="havePrice" onchange="showHaveprice()"><label for="havePrice">Have Price</label>\
+    <input name="s" type="radio" id="noPrice" onchange="showNoprice()"><label for="noPrice">No Price</label>\
+</form>');
+
+
+function showHaveprice(){
+    const o = document.getElementsByClassName("item")
+
+    for ( const  foo of o){
+        if(foo.hasAttribute("price"))foo.style.display = 'inline-table';
+        else foo.style.display = 'none';
+    }   
+}
+
+function showNoprice(){
+    const o = document.getElementsByClassName("item")
+
+    for ( const  foo of o){
+        if(foo.hasAttribute("price") || foo.hasAttribute("sold"))foo.style.display = 'none';
+        else foo.style.display = 'inline-table';
+    }
 }
